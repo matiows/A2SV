f2a70d35cc822677632eec1b02f1143e8ea43159
Matiows <matshimelis@gmail.com>
2021-12-15 00:09:04 +0300

    bug fix

--- Temp/third_max.py
+++ Temp/third_max.py
@@ -1,16 +1,17 @@
-def thirdMax (nums):
-    first=second=third = float('-inf')
-    for num in nums:
-        if num > first:
-            temp = first
-            temp2 = second
-            first = num
-            second = temp
-            third = temp2
-        elif num > second and num < first:
-            temp = second
-            second = num
-            third = temp
-        elif num > third and num < second:
-            third = num
-    return third
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first=second=third = float('-inf')
+        for num in nums:
+            if num > first:
+                temp = first
+                temp2 = second
+                first = num
+                second = temp
+                third = temp2
+            elif num > second and num < first:
+                temp = second
+                second = num
+                third = temp
+            elif num > third and num < second:
+                third = num
+        return third if third != float('-inf') else first
