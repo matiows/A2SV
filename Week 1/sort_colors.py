fd8387f094aba14b8b3b2aa7fda2d25fb6556c27
Matiows <matshimelis@gmail.com>
2021-12-15 05:42:54 +0300

    sort colors first commit

---
+++ Week 1/sort_colors.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortColors(self, a: List[int]) -> None:
+        for i in range(len(a)):
+            for j in range((len(a))-1):
+                if a[j]>a[j+1]:
+                    a[j],a[j+1] = a[j+1],a[j]
