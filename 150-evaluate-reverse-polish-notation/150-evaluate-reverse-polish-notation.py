2c717746b68b84c20630f59eea5078968eb3ae2a
Matiows <51505314+matiows@users.noreply.github.com>
2022-01-17 02:41:20 +0300

    Time: 111 ms (24.08%), Space: 14.8 MB (12.09%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,26 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         reversed_tokens = []
+        stack = []
         for _ in range(len(tokens)):
             reversed_tokens.append(tokens.pop())
-        while len(reversed_tokens) > 1:
-            temp = []
-            nums1 = int(reversed_tokens.pop())
-            nums2 = int(reversed_tokens.pop())
+        for _ in range(len(reversed_tokens)):
             value = reversed_tokens.pop()
-            while value not in ('+', '-', '*', '/') :
-                temp.append(nums1)
-                nums1 = nums2
-                nums2 = int(value)
-                value = reversed_tokens.pop()
-            if value == '+':
-                reversed_tokens.append(nums1+nums2)
-            elif value == '-':
-                 reversed_tokens.append(nums1-nums2)
-            elif value == '*':
-                 reversed_tokens.append(nums1*nums2)
-            elif value == '/':
-                 reversed_tokens.append(nums1/nums2)
-            while len(temp) != 0:
-                reversed_tokens.append(temp.pop())
-        return int(reversed_tokens.pop())
+            if value not in ('+', '-', '*', '/'):
+                stack.append(int(value))
+            else:
+                nums1 = int(stack.pop())
+                nums2 = int(stack.pop())
+                if value == '+':
+                    stack.append(nums2+nums1)
+                elif value == '-':
+                    stack.append(nums2-nums1)
+                elif value == '*':
+                    stack.append(nums2*nums1)
+                elif value == '/':
+                    stack.append(nums2/nums1)
+        return int(stack.pop())
