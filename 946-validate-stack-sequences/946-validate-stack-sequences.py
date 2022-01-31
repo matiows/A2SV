3437e150811bad11ee37b311bff0398c1b38e602
Matiows <51505314+matiows@users.noreply.github.com>
2022-01-31 17:19:30 +0300

    Time: 127 ms (16.53%), Space: 14.2 MB (92.04%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -2,7 +2,6 @@ class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         stack = []
         for i in range(len(pushed)):
-            print(pushed[i] , popped[0])
             if pushed[i] == popped[0]:
                 popped.pop(0)
                 while stack and stack[-1] == popped[0]:
@@ -14,8 +13,6 @@ class Solution:
                     stack.pop()
             else:
                 stack.append(pushed[i])
-            print(stack)
-        print(popped)
         while stack and stack[-1] == popped[0]:
             popped.pop(0)
             stack.pop()
