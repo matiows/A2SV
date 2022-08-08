1ecfaf832f800b5c61187ced1a00b29ede576279
Matiows <51505314+matiows@users.noreply.github.com>
2022-08-08 16:49:56 +0300

    Time: 95 ms (5.47%), Space: 14.3 MB (22.42%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        result = []
+        
+        def backtrack(nums, path):
+            print(nums, path)
+            if len(path) == n:
+                result.append(path)
+                return
+            
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i + 1:], path + [nums[i]])
+                
+        backtrack(nums, [])
+        return result
