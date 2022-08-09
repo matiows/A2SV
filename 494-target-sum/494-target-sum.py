782477c056243900fbea10fb2423c9964af990c8
Matiows <51505314+matiows@users.noreply.github.com>
2022-08-09 23:25:52 +0300

    Time: 399 ms (65.58%), Space: 35.4 MB (43.00%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,15 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        dp = {}
-        dp[0] = 1
-        
-        for num in nums:
-            newSet = defaultdict(int)
+        def dp(idx, t):
+            if (idx, t) in memo:
+                return memo[(idx, t)]
+            if idx == len(nums):
+                if t == 0:
+                    return 1
+                else:
+                    return 0
             
-            for d in dp:
-                newSet[d + num] += dp[d]
-                newSet[d - num] += dp[d]
-            dp = newSet
-        
-        return dp[target]
+            memo[(idx, t)] = dp(idx + 1, t - nums[idx]) + dp(idx + 1, t + nums[idx])
+            return memo[(idx, t)]
+        memo = {}
+        return dp(0, target)
