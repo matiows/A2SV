bf93bfb195ecbcab08c6fdd2ad4ea6e4e1c2a42f
Matiows <51505314+matiows@users.noreply.github.com>
2022-11-02 16:55:46 +0300

    Time: 157 ms (37.86%), Space: 14.4 MB (46.86%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:        
+        answer = []
+        current = [['.']* n for _ in range(n)]
+        col_visited = set()
+        diag_visited = set()
+        inv_diag_visited = set()
+        
+        def checkCell(row, col):
+            if row + col in diag_visited:
+                return False
+            if row - col in inv_diag_visited:
+                return False
+            if col in col_visited:
+                return False
+            
+            return True
+        
+        def putQueen(row):
+            if row == n - 1:
+                temp = []
+                for r in current:
+                    temp.append(''.join(r))
+                    
+                answer.append(temp)
+                return
+            
+            for new_col in range(n):
+                if checkCell(row + 1, new_col):
+                    current[row + 1][new_col] = 'Q'
+                    col_visited.add(new_col)
+                    diag_visited.add(row + 1 + new_col)
+                    inv_diag_visited.add(row + 1 - new_col)
+                    putQueen(row + 1)
+                    current[row + 1][new_col] = '.'
+                    col_visited.remove(new_col)
+                    diag_visited.remove(row + 1 + new_col)
+                    inv_diag_visited.remove(row + 1 - new_col)
+                      
+        putQueen(-1)
+        return answer
+                
+        
