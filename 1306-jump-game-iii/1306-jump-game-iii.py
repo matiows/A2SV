329be62b5190ec0c0928c15dc8a0956ccf9ba7b8
Matiows <51505314+matiows@users.noreply.github.com>
2022-12-16 11:51:38 +0300

    Time: 311 ms (89.98%), Space: 92.1 MB (5.04%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        visited = set()
+        @lru_cache(None)
+        def helper(index):
+            if arr[index] == 0:
+                return True
+            
+            forward = index + arr[index]
+            backward = index - arr[index]
+            
+            if forward not in visited and forward < len(arr):
+                visited.add(forward)
+                if helper(index + arr[index]):
+                    return True
+                
+            if backward not in visited and backward >= 0:
+                visited.add(backward)
+                if helper(index - arr[index]):
+                    return True
+                
+            return False
+        
+        return helper(start)
+        
