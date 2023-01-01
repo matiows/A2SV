e039a5b1358be2f723efab1e03e5c957799f8bed
Matiows <51505314+matiows@users.noreply.github.com>
2023-01-01 21:11:47 +0300

    Time: 31 ms (87.21%), Space: 13.8 MB (74.33%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,26 +2,24 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         hash_map = {}
         sentence = s.split(" ")
-        matched = set()
         i = 0
         
         if len(pattern) != len(sentence):
+            return False
+        
+        if len(set(pattern)) != len(set(sentence)):
             return False
         
         while i < len(pattern):
             ch = pattern[i]
             word = sentence[i]
             
-            if ch in hash_map:
-                if hash_map[ch] != word:
-                    return False
-            else:
-                if word in matched:
-                    return False
-                
+            if ch not in hash_map:
                 hash_map[ch] = word
-                matched.add(word)
-            
+                
+            elif hash_map[ch] != word:
+                return False
+                            
             i += 1
         
         return True
