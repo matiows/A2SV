2486bc16f2e81335a8e919d6735cdcedcf892b20
Matiows <51505314+matiows@users.noreply.github.com>
2023-01-02 08:14:23 +0300

    Time: 32 ms (88.30%), Space: 13.8 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,13 +1,11 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         check = ord(word[0]) < 96
-        if len(word) == 1:
+        caps = 0
+        for ch in word:
+            if ord(ch) < 96:
+                caps += 1
+        if caps == len(word) or caps == 0:
             return True
-        if check and ord(min(word[1:])) > 96:
-            return True
-        if ord(max(word)) < 96:
-            return True
-        if ord(min(word)) > 96:
-            return True
-        return False
+        return caps == 1 and check
         
