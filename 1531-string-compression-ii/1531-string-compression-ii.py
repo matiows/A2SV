ee48e44c0d5a0d72f4f941c10c867a92e3fcdc5c
Matiows <51505314+matiows@users.noreply.github.com>
2023-02-16 23:41:53 +0300

    Time: 2795 ms (32.31%), Space: 358.1 MB (73.85%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        
+        string = list(s)
+        memo = {}
+        
+        def helper(idx, left, last, count):
+            if left < 0:
+                return float('inf')
+            
+            if idx == len(s):
+                return 0
+            
+            if (idx, left, last, count) in memo:
+                return memo[(idx, left, last, count)]
+                
+            answer = 0
+            if s[idx] == last:
+                inc = 0
+                if count == 1 or len(str(count + 1)) > len(str(count)):
+                    inc = 1
+                    
+                answer = inc + helper(idx + 1, left, last, count + 1)
+                    
+            else:
+                pick = 1 + helper(idx + 1, left, s[idx], 1)
+                
+                jump = helper(idx + 1, left - 1, last, count)
+                
+                answer = min(pick, jump)
+            
+            memo[(idx, left, last, count)] = answer
+            return answer
+            
+        return helper(0, k, '', 0)
