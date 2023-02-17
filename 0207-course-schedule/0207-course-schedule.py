25c4c4f79f14fb81768313f8853bc1c381293108
Matiows <51505314+matiows@users.noreply.github.com>
2023-02-17 22:08:23 +0300

    Time: 102 ms (67.63%), Space: 16.9 MB (51.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        incoming = [0] * numCourses
+        coloring = [0] * numCourses
+        courses = []
         
         for des, src in prerequisites:
             graph[src].append(des)
-            incoming[des] += 1
             
-        queue = deque()
-        answer = []
-
-        for idx, degree in enumerate(incoming):
-            if degree == 0:
-                queue.append(idx)
-                
-        while queue:
-            course = queue.popleft()
-            answer.append(course)
             
+        def topDown(course):
+            if coloring[course] == 1:
+                return False
+            
+            if coloring[course] == 2:
+                return True
+            
+            coloring[course] = 1
+            answer = True
             for child in graph[course]:
-                incoming[child] -= 1
+                if child in graph:
+                    answer = answer and topDown(child)
                 
-                if incoming[child] == 0:
-                    queue.append(child)
-                    
-        return len(answer) == numCourses       
+            coloring[course] = 2
+            courses.append(course)
+            return answer
+        
+        answer = True
+        for course in graph:
+            answer = answer and topDown(course)
+            
+        return answer
